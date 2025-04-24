d210633621ac100dd4aab2371d504349eaf40509
0xA3Z <125816778+LowKernelx@users.noreply.github.com>
2025-04-24 12:58:38 +0300

    Add files via upload

---
+++ ASCII-Printer.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+char c;
+
+cout << "=====================================" << endl;    
+cout << "== Project 0xA3Z ACII VALUE" << endl;
+cout << "=====================================" << endl;    
+cout << "input value: ";
+cin >> c;
+
+int aciiValue = (int)c;
+
+cout << "The ASCII value of character " << c << "is: " << aciiValue << endl;
+
+
+
+}
